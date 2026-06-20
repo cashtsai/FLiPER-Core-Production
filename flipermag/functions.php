1c073f5b3a3adc8420c6278c729a2b6483ddd0d6
XCash <xcash@CashCamp.local>
2026-06-20 11:51:08 +0800

    Sync production theme layout state

--- flipermag/functions.php
+++ flipermag/functions.php
@@ -6,11 +6,11 @@ if ( ! isset( $content_width ) ) {
 
 // Add full page thumbnail size
 if ( function_exists( 'add_theme_support' ) ) {
-	add_image_size( 'post-thumb', 900, 500, true );
-	add_image_size( 'home-slider-2x', 552, 352, true );
-	add_image_size( 'home-top-big-2x', 900, 640, true );
-	add_image_size( 'home-top-small-2x', 490, 370, true );
-	add_image_size( 'list-articles-thumb-2x', 720, 458, true );
+	add_image_size( 'post-thumb', 900, 563, true );
+	add_image_size( 'home-slider-2x', 552, 345, true );
+	add_image_size( 'home-top-big-2x', 900, 563, true );
+	add_image_size( 'home-top-small-2x', 490, 306, true );
+	add_image_size( 'list-articles-thumb-2x', 720, 450, true );
 	add_image_size( 'full-post-thumb', 1000, 400, true );
 	add_image_size( 'xl-post-thumb', 1080, 648, true );
 	add_image_size( 'l-post-thumb', 750, 450, true );
@@ -63,6 +63,9 @@ function flipermag_wp_head() {
 
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,600,700|Noto+Serif|EB+Garamond|Zilla+Slab" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;500;600;700&family=IBM+Plex+Mono:wght@400;500;600;700&family=Noto+Serif+TC:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 	<script type="text/javascript">var _jf = _jf || [];_jf.push(['p','56202']);_jf.push(['initAction',true]);_jf.push(['_setFont','notoserifcjktc_extralight','css','.notoserifcjktc_extralight']);_jf.push(['_setFont','notoserifcjktc_extralight','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_extralight','weight',100]);_jf.push(['_setFont','notoserifcjktc_light','css','.notoserifcjktc_light']);_jf.push(['_setFont','notoserifcjktc_light','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_light','weight',200]);_jf.push(['_setFont','notoserifcjktc_regular','css','.notoserifcjktc_regular']);_jf.push(['_setFont','notoserifcjktc_regular','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_regular','weight',300]);_jf.push(['_setFont','notoserifcjktc_medium','css','.notoserifcjktc_medium']);_jf.push(['_setFont','notoserifcjktc_medium','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_medium','weight',400]);_jf.push(['_setFont','notoserifcjktc_semibold','css','.notoserifcjktc_semibold']);_jf.push(['_setFont','notoserifcjktc_semibold','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_semibold','weight',600]);_jf.push(['_setFont','notoserifcjktc_bold','css','.notoserifcjktc_bold']);_jf.push(['_setFont','notoserifcjktc_bold','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_bold','weight',700]);(function(A,p,c,m,l,q,r,h,B,D){var b=A._jf;if(b.constructor!==Object){var t=!0,u=function(a){var f=!0,e;for(e in b)b[e][0]==a&&(f&&(f=f&&!1!==b[e][1].call(b)),b[e]=null,delete b[e])},v=/\S+/g,w=/[\t\r\n\f]/g,C=/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,n="".trim,x=n&&!n.call("\ufeff\u00a0")?function(a){return null==a?"":n.call(a)}:function(a){return null==a?"":(a+"").replace(C,"")},k=function(a){var f,b,g;if("string"===typeof a&&a&&(a=(a||"").match(v)||[],f=h[c]?(" "+h[c]+" ").replace(w," "):" ")){for(g=0;b=a[g++];)0>f.indexOf(" "+b+" ")&&(f+=b+" ");h[c]=x(f)}},d=function(a){var b,e,g;if(0===arguments.length||"string"===typeof a&&a){var d=(a||"").match(v)||[];if(b=h[c]?(" "+h[c]+" ").replace(w," "):""){for(g=0;e=d[g++];)for(;0<=b.indexOf(" "+e+" ");)b=b.replace(" "+e+" "," ");h[c]=a?x(b):""}}},y;b.addScript=y=function(b,f,e,g,d,c){d=d||function(){};c=c||function(){};var a=p.createElement("script"),h=p.getElementsByTagName("script")[0],k,m=!1,l=function(){a.src="";a.parentNode.removeChild(a);a=a.onerror=a.onload=a.onreadystatechange=null};g&&(k=setTimeout(function(){l();c()},g));a.type=f||"text/javascript";a.async=e;a.onload=a.onreadystatechange=function(b,c){m||a.readyState&&!/loaded|complete/.test(a.readyState)||(m=!0,g&&clearTimeout(k),l(),c||d())};a.onerror=function(a,b,d){g&&clearTimeout(k);l();c();return!0};a.src=b;h.parentNode.insertBefore(a,h)};for(var z in b)"initAction"==b[z][0]&&(t=b[z][1]);b.push(["_eventPreload",function(){1==t&&k(m);y(B,null,!1,3E3,null,function(){u("_eventInactived")})}]);b.push(["_eventReload",function(){d(r);d(q);k(l)}]);b.push(["_eventActived",function(){d(m);d(l);k(q)}]);b.push(["_eventInactived",function(){d(m);d(l);k(r)}]);u("_eventPreload")}})(this,this.document,"className","jf-loading","jf-reloading","jf-active","jf-inactive",this.document.getElementsByTagName("html")[0],"//d3gc6cgx8oosp4.cloudfront.net/js/stable/v/5.0.7/id/255049906119");</script>
 
 	<?php
